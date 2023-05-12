1c917102589111281cf2cabbf41317324a300567
Mathieu <mathieu.rasoanaivo@epitech.eu>
2023-05-12 16:12:49 +0400

    modify the injection now we dont necessary need sqlmap

--- src/login.php
+++ src/login.php
@@ -5,6 +5,8 @@ $username = "user_login";
 $password = "password_login";
 $dbname = "db_login";
 
+
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Vérifier la connexion
 if (!$conn) {
@@ -18,39 +20,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$password = $_POST['password'];
 	
 	// Requête pour récupérer l'utilisateur correspondant dans la base de données
-	$sql = "SELECT id, username, password FROM utilisateur WHERE username = '$username'";
+	$sql = "SELECT id, username, password FROM utilisateur WHERE username = ' . $username .'";
 	$result = mysqli_query($conn, $sql);
-	if ($result === false) {
-		die("Erreur de requête: " . mysqli_error($conn));
-	}
-	$row = mysqli_fetch_assoc($result);
-	if ($username === "admin" && $password === $row['password']) {
-		// Rediriger l'utilisateur "ladamin" vers une page spéciale
-		session_start();
-		$_SESSION['id'] = $row['id'];
-		$_SESSION['username'] = $row['username'];
-		// header("Location: page_speciale.php");
-		echo "<p>Bienvenue admin le flag est epictf{uneinjectionparmistantdautre}</p>";
-		exit;
-	} elseif(mysqli_num_rows($result) == 1) {
-		// Utilisateur trouvé, vérifier le mot de passe
-		if($password === $row['password']) {
-			// Mot de passe correct, connecter l'utilisateur
-			session_start();
-			$_SESSION['id'] = $row['id'];
-			$_SESSION['username'] = $row['username'];
-			// header("Location: accueil.php");
-			echo "<p>Bonjour $username.</p>";
-			exit;
-		} else {
-			// Mot de passe incorrect
-			echo "<p>Mot de passe incorrect.</p>";
+	if ($result == TRUE) {
+		echo "bienvenue: $username";
+		foreach($result as $row) {
+			print_r($row);
 		}
 	} else {
-		// Utilisateur non trouvé
-		echo "<p>Utilisateur non trouvé.</p>";
+		echo "<p>Utilisateur incorrect</p>";
 	}
-	
 	mysqli_close($conn);
 }
 ?>
--- src/users.sql
+++ src/users.sql
@@ -6,9 +6,9 @@ CREATE DATABASE IF NOT EXISTS db_login;
 USE db_login;
 
 CREATE TABLE IF NOT EXISTS utilisateur (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(30) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    id INT(9) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(3000) NOT NULL,
+    password VARCHAR(2555) NOT NULL
 );
 
 INSERT INTO utilisateur (username, password) VALUES ('John', 'bondour');
@@ -17,4 +17,5 @@ INSERT INTO utilisateur (username, password) VALUES ('bernado', 'beleck');
 INSERT INTO utilisateur (username, password) VALUES ('jean', 'inzazazf');
 INSERT INTO utilisateur (username, password) VALUES ('royce', 'ozakzazf');
 INSERT INTO utilisateur (username, password) VALUES ('bari', 'azaeveoez');
+INSERT INTO utilisateur (username, password) VALUES ('epictf{fallaitbiencherchercettefoisci}', 'bienjouer');
 INSERT INTO utilisateur (username, password) VALUES ('admin', 'EpiEctfZoRo');
